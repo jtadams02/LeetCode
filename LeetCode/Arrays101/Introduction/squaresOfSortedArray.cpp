f6b3260eeabccbadce05c5bc0ba101748320b159
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-02-20 10:27:27 -0600

    finished Introduction Section of Arrays 101

--- LeetCode/Arrays101/Introduction/squaresOfSortedArray.cpp
+++ LeetCode/Arrays101/Introduction/squaresOfSortedArray.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) 
     {
-        vector<int> squaredVector;
+            vector<int> squaredVector;
 
         for(int i=0;i<nums.size();i++)
         {
@@ -17,6 +17,13 @@ public:
             
             squaredVector.push_back(square); // Add the square to the end of the array
         }
+        /* Debug Printing
+        cout << "Printing squared array" << endl;
+        for(int i=0;i<squaredVector.size();i++)
+        {
+            cout << squaredVector[i] << " ";
+        }
+        */
 
         // Now we just need to sort the new array
 
@@ -24,15 +31,48 @@ public:
         // For counting sort, we first need to find the maximum value in the array, and put every value into the array
         vector<int> countingSort;
         int max = 0;
+
         for(int k=0;k<squaredVector.size();k++)
         {
             if(squaredVector[k]>max){max=squaredVector[k];}
-            countingSort[squaredVector[k]]++;
         }
         // Now lets resize to max+1
         countingSort.resize(max+1);
-        for(int z=0;z<countingSort.size();z++){
-            cout << countingSort[z] << " ";
+        for(int z=0;z<squaredVector.size();z++){
+            int temp = squaredVector[z];
+            countingSort[temp]++;
         }
+
+        /* Print Debugging
+         cout << "\nPrinting counted array" << endl; 
+        for(int i=0;i<countingSort.size();i++)
+        {
+            cout << countingSort[i] << " ";
+        }
+        cout << endl;
+        */
+
+      /*  Made this too complicated
+        int cumSum = 0; // Cumulative Sum
+        for(int xx=0;xx<countingSort.size();xx++){
+            int temp = countingSort[xx];
+            countingSort[xx]+=cumSum;
+            cumSum+=temp;
+        }
+     */
+
+        vector<int>output;
+        int outputIterator = 0; // Use this variable to iterate through the output array and assign values - NOT USED 
+        for(int xx=0;xx<countingSort.size();xx++)
+        {
+            while(countingSort[xx]>0)
+            {
+                output.push_back(xx);
+               // cout << "Pushing " << xx << " to back of array" << endl;
+                countingSort[xx]--;
+            }
+        }
+
+        return output;
     }
 };
